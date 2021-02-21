3455bb9ceb4803552342d499e5d3f6a6ff8ebe48
Hanrui Wang <hanrui@mit.edu>
2021-02-21 16:37:28 -0500

    [minor] fastest is apply_unitary_bmm_no_mat_transpose with implicit contiguous; when batch size is large (>128), unitary with small number of qubit runs slower than with larger (from 1 to 4)

--- examples/core/tools/speed_measure.py
+++ examples/core/tools/speed_measure.py
@@ -120,6 +120,42 @@ def apply_unitary_bmm(state, mat, wires, contiguous=True):
     return new_state
 
 
+def apply_unitary_bmm_no_mat_transpose(state, mat, wires, contiguous=True):
+    device_wires = wires
+
+    if len(mat.shape) > 2:
+        bsz = mat.shape[0]
+        try:
+            assert state.shape[0] == bsz
+        except AssertionError as err:
+            logger.exception(f"Batch size of Quantum Device must be the same "
+                             f"with that of gate unitary matrix")
+            raise err
+
+    devices_dims = [x + 1 for x in device_wires]
+    permute_to = list(range(state.dim()))
+    for d in sorted(devices_dims, reverse=True):
+        del permute_to[d]
+    permute_to = permute_to[:1] + devices_dims + permute_to[1:]
+    permute_back = list(np.argsort(permute_to))
+    original_shape = state.shape
+    permuted = state.permute(permute_to)
+
+    if contiguous:
+        permuted = permuted.contiguous().view(
+            [original_shape[0], mat.shape[-1], -1])
+    else:
+        permuted = permuted.reshape(
+            [original_shape[0], mat.shape[-1], -1])
+
+    new_state = mat.matmul(permuted).view(original_shape).permute(
+        permute_back)
+
+    new_state = new_state.contiguous() if contiguous else new_state
+
+    return new_state
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--pdb', action='store_true', help='pdb')
@@ -129,18 +165,18 @@ if __name__ == '__main__':
     if args.pdb:
         pdb.set_trace()
 
-    batch_size = 64
+    batch_size = 512
     n_wires = 12
     dims = [batch_size] + [2] * n_wires
     device = torch.device('cuda') if args.device == 'gpu' else torch.device(
         'cpu')
     state_in = torch.randn(*dims, dtype=tq.C_DTYPE, device=device)
 
-    run_times = 100
+    run_times = 1000
 
-    is_batch_matrix = False
+    is_batch_matrix = True
 
-    for n_gate_wires in tqdm(range(5, 12)):
+    for n_gate_wires in tqdm(range(1, 10)):
         if is_batch_matrix:
             matrix = torch.randn(batch_size, 2 ** n_gate_wires,
                                  2 ** n_gate_wires,
@@ -153,8 +189,13 @@ if __name__ == '__main__':
 
         res0 = apply_unitary_bmm(state_in, matrix, wires_in, contiguous=True)
         res1 = apply_unitary_einsum(state_in, matrix, wires_in)
-        diff = torch.abs(res1-res0).mean()
-        logger.warning(f"Difference: {diff}")
+        res2 = apply_unitary_bmm_no_mat_transpose(state_in, matrix,
+                                                  wires_in, contiguous=False)
+        diff0 = torch.abs(res0-res1).mean()
+        diff1 = torch.abs(res1-res2).mean()
+        diff2 = torch.abs(res2-res0).mean()
+
+        logger.warning(f"Difference: {diff0, diff1, diff2}")
 
         start = torch.cuda.Event(enable_timing=True)
         end = torch.cuda.Event(enable_timing=True)
@@ -199,4 +240,34 @@ if __name__ == '__main__':
         # Waits for everything to finish running
         torch.cuda.synchronize()
         logger.info(f"bmm no contiguous: {n_gate_wires}:"
+                    f" {start.elapsed_time(end) / run_times}")
+
+        # dry run:
+        for _ in range(5):
+            res = apply_unitary_bmm_no_mat_transpose(
+                state_in, matrix, wires_in, contiguous=False)
+        start.record()
+        for _ in range(run_times):
+            res = apply_unitary_bmm_no_mat_transpose(
+                state_in, matrix, wires_in, contiguous=False)
+        end.record()
+
+        # Waits for everything to finish running
+        torch.cuda.synchronize()
+        logger.info(f"bmm no contiguous no mat transpose: {n_gate_wires}:"
+                    f" {start.elapsed_time(end) / run_times}")
+
+        # dry run:
+        for _ in range(5):
+            res = apply_unitary_bmm_no_mat_transpose(
+                state_in, matrix, wires_in, contiguous=True)
+        start.record()
+        for _ in range(run_times):
+            res = apply_unitary_bmm_no_mat_transpose(
+                state_in, matrix, wires_in, contiguous=True)
+        end.record()
+
+        # Waits for everything to finish running
+        torch.cuda.synchronize()
+        logger.info(f"bmm contiguous no mat transpose: {n_gate_wires}:"
                     f" {start.elapsed_time(end) / run_times}")
