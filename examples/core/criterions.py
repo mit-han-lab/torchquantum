558af4104384d673f54e20a8dba70d78e3d1493d
Hanrui Wang <hanrui@mit.edu>
2021-03-07 14:13:48 -0500

    [minor] add support for layer regression

---
+++ examples/core/criterions.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn.functional as F
+
+
+def complex_mse(output, target):
+    return F.mse_loss(torch.view_as_real(output),
+                      torch.view_as_real(target))
+
+
+def complex_mae(output, target):
+    return (torch.view_as_real(output)
+            - torch.view_as_real(target)).abs().mean()
