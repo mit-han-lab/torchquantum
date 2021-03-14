bb4d9b9fe9ac892af2873666b501d8f6ffad1cec
Hanrui Wang <hanrui@mit.edu>
2021-03-14 16:44:33 -0400

    [minor] statevector match test between tq and qiskit PASSED

--- test/qiskit_plugin_test.py
+++ test/qiskit_plugin_test.py
@@ -6,19 +6,20 @@ import numpy as np
 
 from qiskit import Aer, execute
 from torchpack.utils.logging import logger
-from torchquantum.utils import switch_little_big_endian_matrix
+from torchquantum.utils import (switch_little_big_endian_matrix,
+                                switch_little_big_endian_state)
 from test.static_mode_test import QLayer as AllRandomLayer
 from torchquantum.plugins import tq2qiskit
 from torchquantum.macro import F_DTYPE
 
 
-def find_global_phase(unitary1, unitary2, threshold):
-    for i in range(unitary1.shape[0]):
-        for j in range(unitary1.shape[1]):
+def find_global_phase(mat1, mat2, threshold):
+    for i in range(mat1.shape[0]):
+        for j in range(mat1.shape[1]):
             # find a numerical stable global phase
-            if np.abs(unitary1[i][j]) > threshold and \
-                    np.abs(unitary1[i][j]) > threshold:
-                return unitary2[i][j] / unitary1[i][j]
+            if np.abs(mat1[i][j]) > threshold and \
+                    np.abs(mat1[i][j]) > threshold:
+                return mat2[i][j] / mat1[i][j]
     return None
 
 
@@ -66,6 +67,63 @@ def unitary_tq_vs_qiskit_test():
         except RuntimeError:
             raise RuntimeError
 
+    logger.info(f"PASS tq vs qiskit unitary test")
+
+
+def state_tq_vs_qiskit_test():
+    bsz = 1
+    for n_wires in range(2, 10):
+        q_dev = tq.QuantumDevice(n_wires=n_wires)
+        q_dev.reset_states(bsz=bsz)
+
+        x = torch.randn((1, 100000), dtype=F_DTYPE)
+        q_layer = AllRandomLayer(n_wires=n_wires,
+                                 wires=list(range(n_wires)),
+                                 n_ops_rd=500,
+                                 n_ops_cin=500,
+                                 n_funcs=500,
+                                 qiskit_compatible=True)
+
+        q_layer(q_dev, x)
+        state_tq = q_dev.states.reshape(bsz, -1)
+        state_tq = switch_little_big_endian_state(state_tq.data.numpy())
+
+        # qiskit
+        circ = tq2qiskit(q_layer, x)
+        # Select the StatevectorSimulator from the Aer provider
+        simulator = Aer.get_backend('statevector_simulator')
+
+        # Execute and get counts
+        result = execute(circ, simulator).result()
+        state_qiskit = result.get_statevector(circ)
+
+        stable_threshold = 1e-5
+        try:
+            # WARNING: need to remove the global phase! The qiskit simulated
+            # results sometimes has global phase shift.
+            global_phase = find_global_phase(state_tq,
+                                             np.expand_dims(state_qiskit, 0),
+                                             stable_threshold)
+
+            if global_phase is None:
+                logger.exception(f"Cannot find a stable enough factor to "
+                                 f"reduce the global phase, increase the "
+                                 f"stable_threshold and try again")
+                raise RuntimeError
+
+            assert np.allclose(state_tq * global_phase, state_qiskit,
+                               atol=1e-6)
+            logger.info(f"PASS tq vs qiskit [n_wires]={n_wires}")
+
+        except AssertionError:
+            logger.exception(f"FAIL tq vs qiskit [n_wires]={n_wires}")
+            raise AssertionError
+
+        except RuntimeError:
+            raise RuntimeError
+
+    logger.info(f"PASS tq vs qiskit statevector test")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -80,3 +138,4 @@ if __name__ == '__main__':
         pdb.set_trace()
 
     unitary_tq_vs_qiskit_test()
+    state_tq_vs_qiskit_test()
