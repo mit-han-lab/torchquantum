9032ee5d58c4d01a0c16de07661d9678320a283f
Hanrui Wang <hanrui@mit.edu>
2021-03-25 18:29:27 -0400

    [minor] test general encoder with qfc model 12

--- examples/core/models/q_models.py
+++ examples/core/models/q_models.py
@@ -6,10 +6,9 @@ import torch.nn.functional as F
 import numpy as np
 
 from qiskit import Aer, execute
-from torchquantum.plugins import tq2qiskit, QiskitProcessor
+from torchquantum.plugins import tq2qiskit
 from torchquantum.utils import get_expectations_from_counts
 from torchpack.utils.config import configs
-from torchpack.utils.logging import logger
 from tqdm import tqdm
 
 
@@ -1029,6 +1028,105 @@ class QFCModel11(tq.QuantumModule):
         return x
 
 
+class QFCModel12(tq.QuantumModule):
+    """crx cry crz layers"""
+    class QLayer(tq.QuantumModule):
+        def __init__(self):
+            super().__init__()
+            self.n_wires = 4
+            self.rx_layers = tq.QuantumModuleList()
+            self.ry_layers = tq.QuantumModuleList()
+            self.rz_layers = tq.QuantumModuleList()
+            self.crx_layers = tq.QuantumModuleList()
+            self.cry_layers = tq.QuantumModuleList()
+            self.crz_layers = tq.QuantumModuleList()
+
+            for k in range(configs.model.q_fc12.n_layers):
+                self.rx_layers.append(
+                    tq.Op1QAllLayer(op=tq.RX, n_wires=self.n_wires,
+                                    has_params=True, trainable=True))
+                self.ry_layers.append(
+                    tq.Op1QAllLayer(op=tq.RY, n_wires=self.n_wires,
+                                    has_params=True, trainable=True))
+                self.rz_layers.append(
+                    tq.Op1QAllLayer(op=tq.RZ, n_wires=self.n_wires,
+                                    has_params=True, trainable=True))
+                self.crx_layers.append(
+                    tq.Op2QAllLayer(op=tq.CRX, n_wires=self.n_wires,
+                                    has_params=True, trainable=True,
+                                    circular=True))
+                self.cry_layers.append(
+                    tq.Op2QAllLayer(op=tq.CRY, n_wires=self.n_wires,
+                                    has_params=True, trainable=True,
+                                    circular=True))
+                self.crz_layers.append(
+                    tq.Op2QAllLayer(op=tq.CRZ, n_wires=self.n_wires,
+                                    has_params=True, trainable=True,
+                                    circular=True))
+
+        @tq.static_support
+        def forward(self, q_device: tq.QuantumDevice, x):
+            self.q_device = q_device
+
+            for k in range(configs.model.q_fc12.n_layers):
+                self.rx_layers[k](self.q_device)
+                self.ry_layers[k](self.q_device)
+                self.rz_layers[k](self.q_device)
+                self.crx_layers[k](self.q_device)
+                self.cry_layers[k](self.q_device)
+                self.crz_layers[k](self.q_device)
+
+    def __init__(self):
+        super().__init__()
+        self.n_wires = 4
+        self.q_device = tq.QuantumDevice(n_wires=self.n_wires)
+        self.encoder = tq.GeneralEncoder([
+            {'input_idx': [0], 'func': 'ry', 'wires': [0]},
+            {'input_idx': [1], 'func': 'ry', 'wires': [1]},
+            {'input_idx': [2], 'func': 'ry', 'wires': [2]},
+            {'input_idx': [3], 'func': 'ry', 'wires': [3]},
+            {'input_idx': [4], 'func': 'rz', 'wires': [0]},
+            {'input_idx': [5], 'func': 'rz', 'wires': [1]},
+            {'input_idx': [6], 'func': 'rz', 'wires': [2]},
+            {'input_idx': [7], 'func': 'rz', 'wires': [3]},
+            {'input_idx': [8], 'func': 'rx', 'wires': [0]},
+            {'input_idx': [9], 'func': 'rx', 'wires': [1]},
+            {'input_idx': [10], 'func': 'rx', 'wires': [2]},
+            {'input_idx': [11], 'func': 'rx', 'wires': [3]},
+            {'input_idx': [12], 'func': 'ry', 'wires': [0]},
+            {'input_idx': [13], 'func': 'ry', 'wires': [1]},
+            {'input_idx': [14], 'func': 'ry', 'wires': [2]},
+            {'input_idx': [15], 'func': 'ry', 'wires': [3]}
+        ])
+        self.q_layer = self.QLayer()
+        self.measure = tq.MeasureAll(tq.PauliZ)
+
+    def forward(self, x):
+        bsz = x.shape[0]
+        x = F.avg_pool2d(x, 6).view(bsz, 16)
+        self.encoder(self.q_device, x)
+
+        self.q_layer(self.q_device, x)
+        x = self.measure(self.q_device)
+
+        x = x.reshape(bsz, 2, 2).sum(-1).squeeze()
+        x = F.log_softmax(x, dim=1)
+
+        return x
+
+    def forward_qiskit(self, x):
+        bsz = x.shape[0]
+        x = F.avg_pool2d(x, 6).view(bsz, 16)
+
+        x = self.qiskit_processor.process(
+            self.q_device, self.q_layer, x)
+
+        x = x.reshape(bsz, 2, 2).sum(-1).squeeze()
+        x = F.log_softmax(x, dim=1)
+
+        return x
+
+
 model_dict = {
     'q_quanv0': QuanvModel0,
     'q_quanv1': QuanvModel1,
@@ -1047,5 +1145,6 @@ model_dict = {
     'q_fc9': QFCModel9,
     'q_fc10': QFCModel10,
     'q_fc11': QFCModel11,
+    'q_fc12': QFCModel12,
     'q_qsvt0': QSVT0,
 }
