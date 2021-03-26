4fe5202e40a79f885d805f966cfd939f5034adec
Hanrui Wang <hanrui@mit.edu>
2021-03-26 14:48:42 -0400

    [minor] super q fc4 large design space to test the limit diff sampler

--- examples/core/models/super_models.py
+++ examples/core/models/super_models.py
@@ -230,13 +230,13 @@ class SuperQFCModel3(tq.QuantumModule):
             self.n_wires = n_wires
 
             self.super_layers_all = tq.QuantumModuleList()
-            self.n_blocks = configs.model.super_qfc3.n_blocks
+            self.n_blocks = configs.model.arch.n_blocks
             self.n_front_share_wires = \
-                configs.model.super_qfc3.n_front_share_wires
+                configs.model.arch.n_front_share_wires
             self.n_front_share_ops = \
-                configs.model.super_qfc3.n_front_share_ops
+                configs.model.arch.n_front_share_ops
             self.n_front_share_blocks = \
-                configs.model.super_qfc3.n_front_share_blocks
+                configs.model.arch.n_front_share_blocks
 
             for k in range(self.n_blocks):
                 self.super_layers_all.append(
@@ -363,9 +363,141 @@ class SuperQFCModel3(tq.QuantumModule):
         return space
 
 
+class SuperQFCModel4(tq.QuantumModule):
+    """different from model3: no share front"""
+    class QLayer(tq.QuantumModule):
+        def __init__(self, n_wires):
+            super().__init__()
+            self.n_wires = n_wires
+
+            self.super_layers_all = tq.QuantumModuleList()
+            self.n_blocks = configs.model.arch.n_blocks
+            self.n_front_share_blocks = \
+                configs.model.arch.n_front_share_blocks
+
+            for k in range(self.n_blocks):
+                self.super_layers_all.append(
+                    tq.Super1QLayer(
+                        op=tq.RX, n_wires=self.n_wires,
+                        has_params=True, trainable=True))
+                self.super_layers_all.append(
+                    tq.Super1QLayer(
+                        op=tq.RY, n_wires=self.n_wires,
+                        has_params=True, trainable=True))
+                self.super_layers_all.append(
+                    tq.Super1QLayer(
+                        op=tq.RZ, n_wires=self.n_wires,
+                        has_params=True, trainable=True))
+                self.super_layers_all.append(
+                    tq.Super2QAllLayer(
+                        op=tq.CRX,
+                        n_wires=self.n_wires,
+                        has_params=True,
+                        trainable=True,
+                        jump=1,
+                        circular=True))
+                self.super_layers_all.append(
+                    tq.Super2QAllLayer(
+                        op=tq.CRY,
+                        n_wires=self.n_wires,
+                        has_params=True,
+                        trainable=True,
+                        jump=1,
+                        circular=True))
+                self.super_layers_all.append(
+                    tq.Super2QAllLayer(
+                        op=tq.CRZ,
+                        n_wires=self.n_wires,
+                        has_params=True,
+                        trainable=True,
+                        jump=1,
+                        circular=True))
+            self.sample_n_blocks = None
+
+        def set_sample_arch(self, sample_arch):
+            for k, layer_arch in enumerate(sample_arch[:-1]):
+                self.super_layers_all[k].set_sample_arch(layer_arch)
+            self.sample_n_blocks = sample_arch[-1]
+
+        @tq.static_support
+        def forward(self, q_device: tq.QuantumDevice):
+            self.q_device = q_device
+
+            for k in range(len(self.super_layers_all)):
+                if k < self.sample_n_blocks * 6:
+                    self.super_layers_all[k](self.q_device)
+
+    def __init__(self):
+        super().__init__()
+        self.n_wires = 4
+        self.q_device = tq.QuantumDevice(n_wires=self.n_wires)
+        self.encoder = tq.GeneralEncoder([
+            {'input_idx': [0], 'func': 'ry', 'wires': [0]},
+            {'input_idx': [1], 'func': 'ry', 'wires': [1]},
+            {'input_idx': [2], 'func': 'ry', 'wires': [2]},
+            {'input_idx': [3], 'func': 'ry', 'wires': [3]},
+            {'input_idx': [4], 'func': 'rz', 'wires': [0]},
+            {'input_idx': [5], 'func': 'rz', 'wires': [1]},
+            {'input_idx': [6], 'func': 'rz', 'wires': [2]},
+            {'input_idx': [7], 'func': 'rz', 'wires': [3]},
+            {'input_idx': [8], 'func': 'rx', 'wires': [0]},
+            {'input_idx': [9], 'func': 'rx', 'wires': [1]},
+            {'input_idx': [10], 'func': 'rx', 'wires': [2]},
+            {'input_idx': [11], 'func': 'rx', 'wires': [3]},
+            {'input_idx': [12], 'func': 'ry', 'wires': [0]},
+            {'input_idx': [13], 'func': 'ry', 'wires': [1]},
+            {'input_idx': [14], 'func': 'ry', 'wires': [2]},
+            {'input_idx': [15], 'func': 'ry', 'wires': [3]}
+        ])
+        self.q_layer = self.QLayer(n_wires=self.n_wires)
+        self.measure = tq.MeasureAll(tq.PauliZ)
+
+    def set_sample_arch(self, sample_arch):
+        self.q_layer.set_sample_arch(sample_arch)
+
+    def forward(self, x):
+        bsz = x.shape[0]
+        x = F.avg_pool2d(x, 6).view(bsz, 16)
+        self.encoder(self.q_device, x)
+
+        self.q_layer(self.q_device)
+        x = self.measure(self.q_device).reshape(bsz, 2, 2)
+
+        x = x.sum(-1).squeeze()
+        x = F.log_softmax(x, dim=1)
+
+        return x
+
+    def forward_qiskit(self, x):
+        bsz = x.shape[0]
+        x = F.avg_pool2d(x, 6).view(bsz, 16)
+
+        measured = self.qiskit_processor.process_parameterized(
+            self.q_device, self.encoder, self.q_layer, x)
+        measured = measured.reshape(bsz, 2, 2)
+
+        x = measured.sum(-1).squeeze()
+        x = F.log_softmax(x, dim=1)
+
+        return x
+
+    @property
+    def arch_space(self):
+        space = []
+        for layer in self.q_layer.super_layers_all:
+            space.append(layer.arch_space)
+
+        # for the number of sampled blocks
+        space.append(list(range(self.q_layer.n_front_share_blocks,
+                                self.q_layer.n_blocks + 1)))
+
+        return space
+
+
 model_dict = {
     'super_qfc0': SuperQFCModel0,
     'super_qfc1': SuperQFCModel1,
     'super_qfc2': SuperQFCModel2,
     'super_qfc3': SuperQFCModel3,
+    'super_qfc4': SuperQFCModel4,
 }
