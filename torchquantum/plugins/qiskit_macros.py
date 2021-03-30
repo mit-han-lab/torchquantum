cec0ee2779dd9f2c2a213e87ed2cd71c5bfd1a4f
Hanrui Wang <hanrui@mit.edu>
2021-03-30 00:58:15 -0400

    [minor] add configs

--- torchquantum/plugins/qiskit_macros.py
+++ torchquantum/plugins/qiskit_macros.py
@@ -14,4 +14,4 @@ QISKIT_INCOMPATIBLE_FUNC_NAMES = [
 
 IBMQ_NAMES = ['ibmq_santiago', 'ibmq_athens', 'ibmq_belem',
               'ibmq_quito', 'ibmq_16_melbourne', 'ibmq_lima',
-              'ibmq_5_yorktown', 'ibmq_armonk']
+              'ibmq_5_yorktown', 'ibmq_armonk', 'ibmqx2']
