616683742db7c9d4c95acfeb241d9e77647c9a5b
Hanrui Wang <hanrui@mit.edu>
2021-04-02 14:36:47 -0400

    [minor] update configs and scripts

---
+++ examples/script/eval_subnet_rand_real_x2_opt2.py
@@ -0,0 +1,19 @@
+import subprocess
+from torchpack.utils.logging import logger
+
+
+if __name__ == '__main__':
+    pres = ['python',
+            'examples/eval.py',
+            'examples/configs/mnist/four0123/eval/x2/real/opt2/300.yml',
+            '--jobs=1',
+            '--run-dir']
+    with open('logs/eval_subnet_rand_x2_real_300_opt2_5.txt', 'w') as wfid:
+        for blk in range(8, 9):
+            for rand in range(4):
+                exp = f'runs/mnist.four0123.train.baseline' \
+                      f'.super4digit_arbitrary_fc1.blk8s1.blk{blk}_rand' \
+                      f'{rand}/'
+                logger.info(f"running command {pres + [exp]}")
+
+                subprocess.call(pres + [exp], stderr=wfid)
--- examples/script/get_acc.py
+++ examples/script/get_acc.py
@@ -1,9 +1,8 @@
 if __name__ == '__main__':
     cnt = 0
-    with open('logs/eval_subnet_tq_rand_insuper_expand'
-              '.blk8s1_diff3_chu3_sta40_con1e-3.txt') as rfid:
+    with open('logs/eval_subnet_tq_rand.txt') as rfid:
         for line in rfid:
-            if 'Loss' in line:
+            if 'Accuracy' in line:
                 cnt += 1
                 if cnt % 2:
                     print(eval(line.split(' ')[-1]))
