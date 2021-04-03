dcfd69627b98d4230458d99370420c22030330e9
Hanrui Wang <hanrui@mit.edu>
2021-04-03 16:50:40 -0400

    [minor] update configs and scripts

--- examples/script/eval_subnet_nparams.py
+++ examples/script/eval_subnet_nparams.py
@@ -5,13 +5,12 @@ from torchpack.utils.logging import logger
 if __name__ == '__main__':
     pres = ['python',
             'examples/eval.py',
-            'examples/configs/mnist/four0123/eval/lima/real/opt2/300.yml',
-            '--jobs=1',
+            'examples/configs/mnist/four0123/eval/x2/noise/opt2/all.yml',
+            '--get_n_params=True',
             '--run-dir']
-    with open('logs/eval_subnet_lima_real_300_opt2_blk5.8ratio1.txt', 'w') as \
-            wfid:
-        for blk in [5, 8]:
-            for ratio in ['1']:
+    with open('logs/eval_subnet_nparams.txt', 'w') as wfid:
+        for blk in range(1, 9):
+            for ratio in ['0', '0.25', '0.5', '0.75', '1']:
                 exp = f'runs/mnist.four0123.train.baseline' \
                       f'.super4digit_arbitrary_fc1.blk8s1.blk{blk}_ratio' \
                       f'{ratio}/'
