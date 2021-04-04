3138d596a488f32e0062cc4471c76f57f1b8824b
Hanrui Wang <hanrui@mit.edu>
2021-04-04 15:09:17 -0400

    [minor] bug fix of getting model sample arch

--- examples/train.py
+++ examples/train.py
@@ -183,7 +183,7 @@ def main() -> None:
     #     model.cuda(),
     #     device_ids=[dist.local_rank()],
     #     find_unused_parameters=True)
-    if hasattr(model, 'sample_arch'):
+    if getattr(model, 'sample_arch', None) is not None:
         n_params = model.count_sample_params()
         logger.info(f"Number of sampled params: {n_params}")
 
