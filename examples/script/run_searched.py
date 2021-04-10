fb95bd24bf7cdc89e3149daf86c9ed19d979364d
Hanrui Wang <hanrui@mit.edu>
2021-04-10 13:33:39 -0400

    [minor] update random configs and scripts

--- examples/script/run_searched.py
+++ examples/script/run_searched.py
@@ -7,12 +7,13 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', type=str)
     parser.add_argument('--name', type=str)
     parser.add_argument('--space', type=str)
+    parser.add_argument('--device', type=str)
     args = parser.parse_args()
 
     pres = ['python',
             'examples/eval.py',
             f'examples/configs/'
-            f'{args.dataset}/{args.name}/eval/x2/real/opt2/300.yml',
+            f'{args.dataset}/{args.name}/eval/{args.device}/real/opt2/300.yml',
             '--jobs=5',
             '--run-dir']
 
@@ -21,14 +22,16 @@ if __name__ == '__main__':
     else:
         n_blk = 8
 
-    with open(f'logs/x2/{args.dataset}.{args.name}.{args.space}.txt', 'w') as \
+    with open(f'logs/{args.device}/{args.dataset}.{args.name}'
+              f'.{args.space}.txt',
+              'w') as \
             wfid:
         for mode in [f'plain.blk{n_blk}s1.1.1',
                      f'ldiff.blk{n_blk}s1.1.1_diff7',
                      f'ldiff_blkexpand.blk{n_blk}s1.1.1_diff7_chu3_sta40',
                      f'ldiff_blkexpand.blk{n_blk}s1.1.1_diff7_chu10_sta40',]:
             exp = f'runs/{args.dataset}.{args.name}.train.searched.scratch' \
-                  f'.x2.noise.opt2.setting0.{args.space}.{mode}'
+                  f'.{args.device}.noise.opt2.setting0.{args.space}.{mode}'
 
             logger.info(f"running command {pres + [exp]}")
             subprocess.call(pres + [exp], stderr=wfid)
