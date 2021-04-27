8118ef3d0389d577eb953caa8133b8e1a8a2bdb4
Hanrui Wang <hanrui@mit.edu>
2021-04-27 09:05:26 -0400

    [minor] update plot script

---
+++ examples/script/run_rand_box_12.py
@@ -0,0 +1,28 @@
+import subprocess
+from torchpack.utils.logging import logger
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str)
+    parser.add_argument('--name', type=str)
+    parser.add_argument('--space', type=str)
+    args = parser.parse_args()
+
+    pres = ['python',
+            'examples/eval.py',
+            f'examples/configs/'
+            f'{args.dataset}/{args.name}/eval/x2/real/opt2/300.yml',
+            '--jobs=5',
+            '--run-dir']
+
+    with open(f'logs/x2/box/{args.dataset}.{args.name}.{args.space}.12.txt',
+              'w') as \
+            wfid:
+        for n_params in [12]:
+            for seed in range(4):
+                exp = f'runs/{args.dataset}.{args.name}.train.baseline.' \
+                      f'{args.space}.rand.param{n_params}.seed{seed}'
+
+                logger.info(f"running command {pres + [exp]}")
+                subprocess.call(pres + [exp], stderr=wfid)
