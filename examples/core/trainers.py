5a8ab190af1d5a1c9d43899b01bfde177d4efb8c
Hanrui Wang <hanrui@mit.edu>
2021-05-12 17:36:32 -0400

    [major] add q_noise_aware_trainer

--- examples/core/trainers.py
+++ examples/core/trainers.py
@@ -16,7 +16,7 @@ from torchpack.callbacks.writers import TFEventWriter
 
 
 __all__ = ['QTrainer', 'LayerRegressionTrainer', 'SuperQTrainer',
-           'PruningTrainer']
+           'PruningTrainer', 'QNoiseAwareTrainer']
 
 
 class LayerRegressionTrainer(Trainer):
@@ -326,7 +326,7 @@ class PruningTrainer(Trainer):
         self.init_pruning()
 
     @staticmethod
-    def extract_prunable_parameters(model: nn.Module) -> tuple:
+    def extract_prunable_parameters(model: nn.Module) -> list:
         _parameters_to_prune = [
             (module, "params")
             for _, module in model.named_modules() if isinstance(module,
@@ -470,6 +470,134 @@ class PruningTrainer(Trainer):
         except AttributeError:
             logger.warning(f"No q_layer_op_list or encoder_func_list found, "
                            f"will not save them")
+
+        if self.solution is not None:
+            state_dict['solution'] = self.solution
+            state_dict['score'] = self.score
+
+        try:
+            state_dict['v_c_reg_mapping'] = self.model.measure.v_c_reg_mapping
+        except AttributeError:
+            logger.warning(f"No v_c_reg_mapping found, will not save it.")
+        return state_dict
+
+    def _load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        # self.model.load_state_dict(state_dict['model'])
+        self.optimizer.load_state_dict(state_dict['optimizer'])
+        self.scheduler.load_state_dict(state_dict['scheduler'])
+
+
+class QNoiseAwareTrainer(Trainer):
+    def __init__(self, *, model: nn.Module, criterion: Callable,
+                 optimizer: Optimizer, scheduler: Scheduler) -> None:
+        self.model = model
+        self.legalized_model = None
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.solution = None
+        self.score = None
+
+    def _before_epoch(self) -> None:
+        self.model.train()
+        # adjust the noise magnitude
+        if self.model.noise_model_tq is not None:
+            self.model.noise_model_tq.mode = 'train'
+            self.model.noise_model_tq.adjust_noise(self.epoch_num)
+
+    def run_step(self, feed_dict: Dict[str, Any], legalize=False) -> Dict[
+            str, Any]:
+        output_dict = self._run_step(feed_dict, legalize=legalize)
+        return output_dict
+
+    def _run_step(self, feed_dict: Dict[str, Any], legalize=False) -> Dict[
+            str, Any]:
+        if configs.run.device == 'gpu':
+            inputs = feed_dict[configs.dataset.input_name].cuda(
+                non_blocking=True)
+            targets = feed_dict[configs.dataset.target_name].cuda(
+                non_blocking=True)
+        else:
+            inputs = feed_dict[configs.dataset.input_name]
+            targets = feed_dict[configs.dataset.target_name]
+        if legalize:
+            outputs = self.legalized_model(inputs)
+        else:
+            outputs = self.model(inputs)
+        loss = self.criterion(outputs, targets)
+        nll_loss = loss.item()
+        unitary_loss = 0
+
+        if configs.regularization.unitary_loss:
+            unitary_loss = get_unitary_loss(self.model)
+            if configs.regularization.unitary_loss_lambda_trainable:
+                loss += self.model.unitary_loss_lambda[0] * unitary_loss
+            else:
+                loss += configs.regularization.unitary_loss_lambda * \
+                        unitary_loss
+
+        if loss.requires_grad:
+            for k, group in enumerate(self.optimizer.param_groups):
+                self.summary.add_scalar(f'lr/lr_group{k}', group['lr'])
+            self.summary.add_scalar('loss', loss.item())
+            self.summary.add_scalar('nll_loss', nll_loss)
+            if self.model.noise_model_tq is not None:
+                if self.model.noise_model_tq.noise_total_prob is not None:
+                    noise_total_prob = \
+                        self.model.noise_model_tq.noise_total_prob
+                else:
+                    noise_total_prob = -1
+                self.summary.add_scalar('noise_total_prob', noise_total_prob)
+
+            if getattr(self.model, 'sample_arch', None) is not None:
+                for writer in self.summary.writers:
+                    if isinstance(writer, TFEventWriter):
+                        writer.writer.add_text(
+                            'sample_arch', str(self.model.sample_arch),
+                            self.global_step)
+
+            if configs.regularization.unitary_loss:
+                if configs.regularization.unitary_loss_lambda_trainable:
+                    self.summary.add_scalar(
+                        'u_loss_lambda',
+                        self.model.unitary_loss_lambda.item())
+                else:
+                    self.summary.add_scalar(
+                        'u_loss_lambda',
+                        configs.regularization.unitary_loss_lambda)
+                self.summary.add_scalar('u_loss', unitary_loss.item())
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+        return {'outputs': outputs, 'targets': targets}
+
+    def _after_epoch(self) -> None:
+        self.model.eval()
+        self.scheduler.step()
+        if configs.legalization.legalize:
+            if self.epoch_num % configs.legalization.epoch_interval == 0:
+                legalize_unitary(self.model)
+
+        # set to eval mode, will not add noise
+        if self.model.noise_model_tq is not None:
+            self.model.noise_model_tq.mode = 'eval'
+
+    def _after_step(self, output_dict) -> None:
+        if configs.legalization.legalize:
+            if self.global_step % configs.legalization.step_interval == 0:
+                legalize_unitary(self.model)
+
+    def _state_dict(self) -> Dict[str, Any]:
+        state_dict = dict()
+        # need to store model arch because of randomness of random layers
+        state_dict['model_arch'] = self.model
+        state_dict['model'] = self.model.state_dict()
+        state_dict['optimizer'] = self.optimizer.state_dict()
+        state_dict['scheduler'] = self.scheduler.state_dict()
+        if getattr(self.model, 'sample_arch', None) is not None:
+            state_dict['sample_arch'] = self.model.sample_arch
 
         if self.solution is not None:
             state_dict['solution'] = self.solution
