eb5102a08125e92d07e9053cbfcbff6c3b371946
Hanrui Wang <hanrui@mit.edu>
2022-10-10 10:56:20 -0400

    [minor] 0.1.4 after fixing bugs

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='torchquantum',
-      version='0.1.2',
+      version='0.1.4',
       description='A PyTorch-centric hybrid classical-quantum dynamic '
                   'neural networks framework.',
       url='https://github.com/mit-han-lab/torchquantum',
--- torchquantum/__init__.py
+++ torchquantum/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = 'Hanrui Wang'
 
 from .macro import *
