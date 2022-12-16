1463c5e20da06379232eb9c506c7eb684488a59c
yezhuoyang <737475845@qq.com>
2022-12-16 19:54:35 +0800

    Add kraus operator class.

---
+++ torchquantum/kraus.py
@@ -0,0 +1,13 @@
+
+
+
+class Kraus(tq.QuantumModule):
+    """The class for karus operators."""
+    
+    def __init__(self,
+                 has_params: bool = False,
+                 trainable: bool = False,
+                 init_params=None,
+                 n_wires=None,
+                 wires=None):
+        return
