06fc2cd5175b015967275c605f9135cd03906d6d
dikshantdulal10 <ddula@softserveinc.com>
2023-02-27 23:08:58 +0800

    updated the trainable layer

--- examples/qaoa/max_cut.py
+++ examples/qaoa/max_cut.py
@@ -7,6 +7,8 @@ import numpy as np
 
 from torchquantum.functional import mat_dict
 
+# from torchquantum.plugins import tq2qiskit, qiskit2tq
+
 seed = 0
 random.seed(seed)
 np.random.seed(seed)
@@ -73,26 +75,48 @@ class MAXCUT(tq.QuantumModule):
 
         self.q_device = tq.QuantumDevice(n_wires=n_wires)
 
-        # self.rx0 = tq.RX(has_params=False, trainable=False)
-        # self.rz0 = tq.RZ(has_params=False, trainable=False)
+        self.betas = torch.nn.Parameter(0.01 * torch.rand(self.n_layers))
+        self.gammas = torch.nn.Parameter(0.01 * torch.rand(self.n_layers))
 
-        self.betas = torch.nn.Parameter(torch.rand(self.n_layers))
-        self.gammas = torch.nn.Parameter(torch.rand(self.n_layers))
-
-    def mixer_n_entangler(self, edge):
+    def mixer(self, beta):
         """
         Apply the single rotation and entangling layer of the QAOA ansatz.
         mixer = exp(-i * beta * sigma_x)
-        entangler = exp(-i * gamma * (1 - sigma_z * sigma_z)/2)
         """
         for wire in range(self.n_wires):
-            for (beta, gamma) in zip(self.betas, self.gammas):
-                # mixer
-                tqf.rx(self.q_device, wires=wire, params=2 * beta.unsqueeze(0))
-                # entangler
-                tqf.cx(self.q_device, [edge[0], edge[1]])
-                tqf.rz(self.q_device, wires=edge[1], params=gamma.unsqueeze(0))
-                tqf.cx(self.q_device, [edge[0], edge[1]])
+            tqf.rx(
+                self.q_device,
+                wires=wire,
+                params=2 * beta.unsqueeze(0),
+                static=self.static_mode,
+                parent_graph=self.graph,
+            )
+
+    def entangler(self, gamma):
+        """
+        Apply the single rotation and entangling layer of the QAOA ansatz.
+        entangler = exp(-i * gamma * (1 - sigma_z * sigma_z)/2)
+        """
+        for edge in self.input_graph:
+            tqf.cx(
+                self.q_device,
+                [edge[0], edge[1]],
+                static=self.static_mode,
+                parent_graph=self.graph,
+            )
+            tqf.rz(
+                self.q_device,
+                wires=edge[1],
+                params=gamma.unsqueeze(0),
+                static=self.static_mode,
+                parent_graph=self.graph,
+            )
+            tqf.cx(
+                self.q_device,
+                [edge[0], edge[1]],
+                static=self.static_mode,
+                parent_graph=self.graph,
+            )
 
     def edge_to_PauliString(self, edge):
         # construct pauli string
@@ -108,11 +132,18 @@ class MAXCUT(tq.QuantumModule):
         """
         execute the quantum circuit
         """
+        tqf.h(
+            self.q_device,
+            wires=list(range(self.n_wires)),
+            static=self.static_mode,
+            parent_graph=self.graph,
+        )
 
-        tqf.h(self.q_device, wires=list(range(self.n_wires)))
-        for k in range(self.n_layers):
-            self.mixer_n_entangler(self.input_graph[k])
+        for i in range(self.n_layers):
+            self.entangler(self.gammas[i])
+            self.mixer(self.betas[i])
 
+    @tq.static_support
     def forward(self, measure_all=False):
         """
         Apply the QAOA ansatz and only measure the edge qubit on z-basis.
@@ -122,7 +153,7 @@ class MAXCUT(tq.QuantumModule):
         self.q_device.reset_states(1)
         self.circuit()
         # states = self.q_device.get_states_1d()
-        print(tq.measure(self.q_device, n_shots=1024))
+        # print(tq.measure(self.q_device, n_shots=1024))
         # compute the expectation value
         if measure_all is False:
             expVal = 0
@@ -217,7 +248,7 @@ def param_shift_optimize(model, n_steps=10, step_size=0.1):
         for param, grad in zip(param_list, grad_list):
             # modify the parameters and ensure that there are no multiple views
             param.copy_(param - step_size * grad)
-        if step % 2 == 0:
+        if step % 5 == 0:
             print("Step: {}, Cost Objective: {}".format(step, loss.item()))
 
         print(
@@ -230,15 +261,27 @@ def param_shift_optimize(model, n_steps=10, step_size=0.1):
 
 def main():
     # create a input_graph
-    input_graph = [(0, 1), (3, 0), (1, 2), (2, 3)]
+    input_graph = [(0, 1), (0, 3), (1, 2), (2, 3)]
     n_wires = 4
-    n_layers = 2
+    n_layers = 1
     model = MAXCUT(n_wires=n_wires, input_graph=input_graph, n_layers=n_layers)
+    # circ = tq2qiskit(tq.QuantumDevice(n_wires=4), model)
+    # print("The circuit is", circ.draw(output="mpl"))
+    # circ.draw(output="mpl")
     # use backprop
-    backprop_optimize(model, n_steps=500, lr=0.01)
+    backprop_optimize(model, n_steps=30, lr=0.1)
     # use parameter shift rule
     # param_shift_optimize(model, n_steps=10, step_size=0.1)
 
+
+"""
+Notes:
+1. input_graph = [(0, 1), (3, 0), (1, 2), (2, 3)], mixer 1st & entangler 2nd, n_layers =2, answer = correct.
+                                                                              n_layers =1, answer = wrong, same probs for all edges.
+2. input_graph = [(0, 1), (3, 0), (1, 2), (2, 3)], mixer 2nd & entangler 1st, n_layers =2, answer = correct.
+                                                                              n_layers =1, answer = wrong, same probs for all edges.
+
+"""
 
 if __name__ == "__main__":
     main()
