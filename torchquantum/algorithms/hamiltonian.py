36106978f9ad0e1f5cc70a9e34e8df173ed676f4
Denise/Denča <95103224+denvitko@users.noreply.github.com>
2023-06-03 16:20:35 +0200

    added docstrings
    Added docstrings. Using Google Docstrings style.

--- torchquantum/algorithms/hamiltonian.py
+++ torchquantum/algorithms/hamiltonian.py
@@ -12,11 +12,40 @@ from torchquantum.vqe_utils import parse_hamiltonian_file
 __all__ = ["Hamiltonian"]
 
 class Hamiltonian(object):
+    """The Hamiltonian.
+    
+    This class provides functionality to process and initialize a Hamiltonian.
+
+    Attributes:
+        hamil_info (dict): Information about the Hamiltonian.
+        n_wires (int): Number of wires in the Hamiltonian.
+        
+    Methods:
+        __init__(hamil_info): Initializes the Hamiltonian object.
+        process_hamil_info: Process the Hamiltonian information.
+        from_file: Create a Hamiltonian from a file.
+    """
+    
     def __init__(self, hamil_info) -> None:
+        """Initialize the Hamiltonian object.
+
+        Args:
+            hamil_info (dict): Information about the Hamiltonian
+        """
+        
         self.hamil_info = self.process_hamil_info(hamil_info)
         self.n_wires = hamil_info["n_wires"]
 
     def process_hamil_info(self, hamil_info):
+        """Process the Hamiltonian information.
+
+        Args:
+            hamil_info (dict): Information about the Hamiltonian
+
+        Returns:
+            dict: processed Hamiltonian information
+        """
+        
         hamil_list = hamil_info["hamil_list"]
         n_wires = hamil_info["n_wires"]
         all_info = []
@@ -35,6 +64,15 @@ class Hamiltonian(object):
 
     @classmethod
     def from_file(cls, file_path):
+        """Create a Hamiltonian object from a file.
+
+        Args:
+            file_path (str): Path to the file containing Hamiltonian information
+
+        Returns:
+            Hamiltonian: the created Hamiltonian object
+        """
+        
         hamil_info = parse_hamiltonian_file(file_path)
         return cls(hamil_info)
     
