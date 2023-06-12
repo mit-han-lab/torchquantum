2a099795b403671fd29b053d630b5768cae2bfc1
Denise/Denča <95103224+denvitko@users.noreply.github.com>
2023-06-12 18:32:34 +0200

    added docstrings

--- torchquantum/plugins/qiskit_pulse.py
+++ torchquantum/plugins/qiskit_pulse.py
@@ -8,6 +8,23 @@ from .qiskit_macros import IBMQ_PNAMES
 
 
 def circ2pulse(circuits, name):
+    """
+    Convert a circuit to a pulse schedule using the specified backend.
+
+    Args:
+        circuits (QuantumCircuit): The input quantum circuit.
+        name (str): The name of the backend.
+
+    Returns:
+        None.
+
+    Example:
+        >>> qc = QuantumCircuit(2)
+        >>> qc.h(0)
+        >>> qc.cx(0, 1)
+        >>> circ2pulse(qc, 'ibmq_oslo')
+    """
+    
     if name in IBMQ_PNAMES:
         backend = name()
         with pulse.build(backend) as pulse_tq:
