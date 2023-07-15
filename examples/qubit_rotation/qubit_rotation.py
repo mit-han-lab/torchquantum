bcf9d01a5834a5030e2bcd86226d93f82ad173cf
GenericP3rson <genericp3rson@gmail.com>
2023-07-14 22:01:55 -0400

    [minor] updated format with black for lint tests

--- examples/qubit_rotation/qubit_rotation.py
+++ examples/qubit_rotation/qubit_rotation.py
@@ -1,16 +1,18 @@
-'''
+"""
 Qubit Rotation Optimization, adapted from https://pennylane.ai/qml/demos/tutorial_qubit_rotation
-'''
+"""
 
 # import dependencies
 import torchquantum as tq
 import torch
 from torchquantum.measurement import expval_joint_analytical
 
+
 class OptimizationModel(torch.nn.Module):
-    '''
+    """
     Circuit with rx and ry gate
-    '''
+    """
+
     def __init__(self):
         super().__init__()
         self.rx0 = tq.RX(has_params=True, trainable=True, init_params=0.011)
@@ -25,11 +27,11 @@ class OptimizationModel(torch.nn.Module):
         self.ry0(qdev, wires=0)
 
         # return the analytic expval from Z
-        return expval_joint_analytical(qdev, 'Z')
+        return expval_joint_analytical(qdev, "Z")
+
 
 # train function to get expval as low as possible (ideally -1)
 def train(model, device, optimizer):
-
     outputs = model()
     loss = outputs
     optimizer.zero_grad()
@@ -37,6 +39,7 @@ def train(model, device, optimizer):
     optimizer.step()
 
     return loss.item()
+
 
 # main function to run the optimization
 def main():
@@ -63,4 +66,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
