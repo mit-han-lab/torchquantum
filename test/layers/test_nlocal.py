ff10729176e8db9a9b2336bfd327e42a5b1cba0f
GenericP3rson <genericp3rson@gmail.com>
2023-11-11 20:36:28 -0500

    adding tests for other nlocal circuits

---
+++ test/layers/test_nlocal.py
@@ -0,0 +1,93 @@
+import torchquantum as tq
+from qiskit.circuit.library import TwoLocal, EfficientSU2, ExcitationPreserving, PauliTwoDesign, RealAmplitudes
+
+def compare_tq_to_qiskit(tq_circuit, qiskit_circuit, instance_info = ""):
+    """
+    helper function to compare if tq and qiskit have the same gates configuration
+    """
+    
+    qiskit_ops = []
+    for bit in qiskit_circuit.decompose():
+        wires = []
+        for qu in bit.qubits:
+            wires.append(qu.index)
+        qiskit_ops.append(
+            {
+                "name": bit.operation.name,
+                "wires": tuple(wires),
+            }
+        )
+
+    # create operations list
+    tq_ops = [
+        {
+            "name": op["name"],
+            "wires": (op["wires"],)
+            if isinstance(op["wires"], int)
+            else tuple(op["wires"]),
+        }
+        for op in tq_circuit.op_history
+    ]
+
+    # create tuples (NOTE: WILL LOSE ORDER SO NOT ENTIRELY CORRECT)
+    tq_ops_tuple = {tuple(op) for op in tq_ops}
+    qiskit_ops_tuple = {tuple(op) for op in qiskit_ops}
+
+    # assert if they are the same
+    assert len(tq_ops) == len(
+        qiskit_ops
+    ), f"operations are varying lengths for {instance_info}"
+    assert (
+        tq_ops_tuple == qiskit_ops_tuple
+    ), f"operations do not match for {instance_info}"
+
+## TEST TWOLOCAL
+
+# iterate through different parameters to test
+for entanglement_type in ("linear", "circular", "full"):
+    for n_wires in (3, 5, 10):
+        for reps in range(1, 5):
+            # create the TQ circuit
+            tq_two = tq.layer.TwoLocal(
+                [tq.RY, tq.RZ],
+                [tq.CZ],
+                arch={"n_wires": n_wires},
+                entanglement_layer=entanglement_type,
+                reps=reps,
+            )
+            qdev = tq.QuantumDevice(n_wires, record_op=True)
+            tq_two(qdev)
+
+            # create the qiskit circuit
+            qiskit_two = TwoLocal(
+                n_wires,
+                ["ry", "rz"],
+                "cz",
+                entanglement_type,
+                reps=reps,
+                insert_barriers=False,
+            )
+            
+            # compare the circuits
+            test_info = f"{entanglement_type} with {n_wires} wires and {reps} reps"
+            compare_tq_to_qiskit(qdev, qiskit_two)
+
+
+## TEST OTHER CIRCUITS
+
+tq_to_qiskit = {
+    "EfficientSU2": (tq.layer.EfficientSU2, EfficientSU2),
+    "ExcitationPreserving": (tq.layer.ExcitationPreserving, ExcitationPreserving),
+    "RealAmplitudes": (tq.layer.RealAmplitudes, RealAmplitudes),
+}
+
+# run all the tests
+for circuit_name in tq_to_qiskit:
+    tq_instance, qiskit_instance = tq_to_qiskit[circuit_name]
+    for n_wires in range(2, 5):
+        tq_circuit = tq_instance({"n_wires": n_wires})
+        circuit = qiskit_instance(n_wires)
+        qdev = tq.QuantumDevice(n_wires, record_op=True)
+        tq_circuit(qdev)
+        compare_tq_to_qiskit(qdev, circuit, f"{circuit_name} with {n_wires} wires")
+
