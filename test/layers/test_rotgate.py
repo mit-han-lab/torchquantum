56f29adf59bff2e3b015a23db56753a5e0947cf4
GenericP3rson <genericp3rson@gmail.com>
2023-12-22 22:00:25 -0600

    minor reformatting of the layers

--- test/layers/test_rotgate.py
+++ test/layers/test_rotgate.py
@@ -17,39 +17,40 @@ all_pairs = [
     {"qiskit": GRZ, "tq": tq.layer.GlobalRZ, "params": 1},
 ]
 
-ITERATIONS = 10
+ITERATIONS = 2
 
-# test each pair
-for pair in all_pairs:
-    # test 2-5 wires
-    for num_wires in range(2, 5):
-        # try multiple random parameters
-        for _ in range(ITERATIONS):
-            # generate random parameters
-            params = [
-                np.random.uniform(-2 * np.pi, 2 * np.pi) for _ in range(pair["params"])
-            ]
+def test_rotgates():
+    # test each pair
+    for pair in all_pairs:
+        # test 2-5 wires
+        for num_wires in range(2, 5):
+            # try multiple random parameters
+            for _ in range(ITERATIONS):
+                # generate random parameters
+                params = [
+                    np.random.uniform(-2 * np.pi, 2 * np.pi) for _ in range(pair["params"])
+                ]
 
-            # create the qiskit circuit
-            qiskit_circuit = pair["qiskit"](num_wires, *params)
+                # create the qiskit circuit
+                qiskit_circuit = pair["qiskit"](num_wires, *params)
 
-            # get the unitary from qiskit
-            backend = Aer.get_backend("unitary_simulator")
-            result = execute(qiskit_circuit, backend).result()
-            unitary_qiskit = result.get_unitary(qiskit_circuit)
+                # get the unitary from qiskit
+                backend = Aer.get_backend("unitary_simulator")
+                result = execute(qiskit_circuit, backend).result()
+                unitary_qiskit = result.get_unitary(qiskit_circuit)
 
-            # create tq circuit
-            qdev = tq.QuantumDevice(num_wires)
-            tq_circuit = pair["tq"](num_wires, *params)
-            tq_circuit(qdev)
+                # create tq circuit
+                qdev = tq.QuantumDevice(num_wires)
+                tq_circuit = pair["tq"](num_wires, *params)
+                tq_circuit(qdev)
 
-            # get the unitary from tq
-            unitary_tq = tq_circuit.get_unitary(qdev)
-            unitary_tq = switch_little_big_endian_matrix(unitary_tq.data.numpy())
+                # get the unitary from tq
+                unitary_tq = tq_circuit.get_unitary(qdev)
+                unitary_tq = switch_little_big_endian_matrix(unitary_tq.data.numpy())
 
-            # phase?
-            phase = find_global_phase(unitary_tq, unitary_qiskit, 1e-4)
+                # phase?
+                phase = find_global_phase(unitary_tq, unitary_qiskit, 1e-4)
 
-            assert np.allclose(
-                unitary_tq * phase, unitary_qiskit, atol=1e-6
-            ), f"{pair} not equal with {params=}!"
+                assert np.allclose(
+                    unitary_tq * phase, unitary_qiskit, atol=1e-6
+                ), f"{pair} not equal with {params=}!"
