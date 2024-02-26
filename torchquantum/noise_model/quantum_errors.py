fb645b9d98d397711b43567d0920cac30ca8b55c
Zhuoyang Ye <yezhuoyang98@gmail.com>
2024-02-25 17:15:27 -0800

    [Class] Add Kraus, noise_channel,quantum_errors class.

---
+++ torchquantum/noise_model/quantum_errors.py
@@ -0,0 +1,198 @@
+"""
+MIT License
+
+Copyright (c) 2020-present TorchQuantum Authors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import numpy as np
+import torch
+import torchquantum as tq
+
+
+class QuantumError(object):
+    """
+        A class for describing quantum error
+
+        Methods:
+
+
+    """
+    def __init__(self,
+                 wires):
+        pass
+
+
+
+class KrausError(QuantumError):
+    """
+        A class for describing quantum error
+
+        Methods:
+
+
+    """
+    def __init__(self,
+                 wires):
+        pass
+
+
+
+class mixed_unitary_error(QuantumError):
+    """
+        A class for describing quantum error
+
+        Methods:
+
+
+    """
+    def __init__(self,
+                 wires):
+        pass
+
+
+class coherent_unitary_error(QuantumError):
+    """
+        A class for describing quantum error
+
+        Methods:
+
+
+    """
+
+    def __init__(self,
+                 wires):
+        pass
+
+
+
+
+class pauli_error(QuantumError):
+    """
+        A class for describing quantum error
+
+        Methods:
+
+
+    """
+
+    def __init__(self,
+                 wires):
+        pass
+
+
+
+class depolarizing_error(QuantumError):
+    """
+        A class for describing quantum error
+
+        Methods:
+
+
+    """
+
+    def __init__(self,
+                 wires):
+        pass
+
+
+
+class reset_error(QuantumError):
+    """
+        A class for describing quantum error
+
+        Methods:
+
+
+    """
+
+    def __init__(self,
+                 wires):
+        pass
+
+
+
+class thermal_relaxation_error(QuantumError):
+    """
+        A class for describing quantum error
+
+        Methods:
+
+
+    """
+
+    def __init__(self,
+                 wires):
+        pass
+
+
+
+class phase_amplitude_damping_error(QuantumError):
+    """
+        A class for describing quantum error
+
+        Methods:
+
+
+    """
+
+    def __init__(self,
+                 wires):
+        pass
+
+
+class amplitude_damping_error(QuantumError):
+    """
+        A class for describing quantum error
+
+        Methods:
+
+
+    """
+
+    def __init__(self,
+                 wires):
+        pass
+
+
+
+class phase_damping_error(QuantumError):
+    """
+        A class for describing quantum error
+
+        Methods:
+
+
+    """
+
+    def __init__(self,
+                 wires):
+        pass
+
+
+
+
+
+
+
+
+
+
+
