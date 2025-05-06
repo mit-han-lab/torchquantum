21460ea447a67f17022d64658feb5891799f8faf
Khaldoon Ghanem <kghanem@nvidia.com>
2025-05-06 15:03:01 +0000

    Update copyright headers

--- torchquantum/plugin/cuquantum/expectation.py
+++ torchquantum/plugin/cuquantum/expectation.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # SPDX-License-Identifier: MIT
 
